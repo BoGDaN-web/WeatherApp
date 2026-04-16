834bcaf965183f56f02d31d195fb18cb14c928d5
Bogdan <amelkovicbogdan@gmail.com>
2026-04-16 14:17:11 +0300

    cleanup project structure and add readme

---
+++ WeatherApp/Core/Extensions/UIView+Constraints.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension UIView {
+    func pinToSuperview(insets: UIEdgeInsets = .zero) {
+        guard let superview else { return }
+
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor, constant: insets.top),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: insets.left),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -insets.right),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -insets.bottom)
+        ])
+    }
+}
